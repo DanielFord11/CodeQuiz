a3983355853bfd6dc25251f6f09bee6478a98c4f
iigonzoii <a.gonzalez418@gmail.com>
2021-01-20 17:31:38 -0700

    pseudo code, for loop, and innerhtml

--- questions.js
+++ questions.js
@@ -1,6 +1,8 @@
+var questionToShowIndex = 0
+
 var questions = [ {
     // include if else statement if they get right score and move forward, else deduct time and move forward in my functions on the main
-    title: "which one of these is not a data type?",
+    question: "which one of these is not a data type?",
     choices: [
     "string",
     "boolean",
@@ -10,12 +12,12 @@ var questions = [ {
     answer: "div"
     },
     {
-    title: "an array is represented by which of these characters",
+    question: "an array is represented by which of these characters",
     choices: ["[]", "{}", "()", "#"],
     answer: "[]"
     },
     {
-    title: "true and false represent what type of data type?",
+    question: "true and false represent what type of data type?",
     choices: [
     "boolean",
     "number",
@@ -24,3 +26,17 @@ var questions = [ {
     ],
     answer: "boolean"
     },]
+
+    // console.log(questions[0].question);
+    document.getElementById(`questions`).innerHTML = questions[questionToShowIndex].question
+
+    for (let i = 0; i < questions[questionToShowIndex].choices.length; i++){
+        var singleChoice = document.createElement(`button`)
+        singleChoice.innerText = (questions[questionToShowIndex].choices[i])
+         singleChoice.classList.add(`choice`)
+         document.getElementById(`choices`).appendChild(singleChoice)
+    //    console.log(singleChoice);
+        // console.log(questions[questionToShowIndex].choices[i]);
+    }
+
+    // document.getElementById(`questions`).innerHTML = questions[questionsToShowIndex]
